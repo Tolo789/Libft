47b6c9385b02c1d2a92c790513398ea685917a99
Claudio Mutti <cmutti@student.42.fr>
2016-02-22 16:35:18 +0100

    Update ft_putnbr.c

--- libft/basic_src/ft_putnbr.c
+++ libft/basic_src/ft_putnbr.c
@@ -12,22 +12,19 @@
 
 #include "libft.h"
 
-static int	ft_abs(int n)
-{
-	if (n < 0)
-		n = -n;
-	return (n);
-}
-
 void		ft_putnbr(int n)
 {
 	if (n < 0)
+	{	
 		ft_putchar('-');
-	if (n < 10 && n > -10)
-		ft_putchar('0' + ft_abs(n));
+		ft_putchar('0' - n % 10);
+		ft_putnbr(n / -10);
+	}
+	if (n < 10)
+		ft_putchar('0' + n);
 	else
 	{
-		ft_putnbr(ft_abs(n / 10));
-		ft_putchar('0' + ft_abs(n % 10));
+		ft_putnbr(n / 10);
+		ft_putchar('0' + n % 10);
 	}
 }
