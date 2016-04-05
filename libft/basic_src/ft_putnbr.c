9dab004f8b52a96e26876850db3e51e44df2fca3
Claudio Mutti <cmutti@student.42.fr>
2016-04-06 00:01:44 +0200

    Update ft_putnbr.c

--- libft/basic_src/ft_putnbr.c
+++ libft/basic_src/ft_putnbr.c
@@ -14,13 +14,19 @@
 
 void		ft_putnbr(int n)
 {
+	
 	if (n < 0)
+	{
 		ft_putchar('-');
-	if (n < 10 && n > -10)
-		ft_putchar('0' + ft_abs(n));
+		if (n < -9)
+			ft_putnbr(-(n / 10));
+		ft_putchar('0' - n % 10);
+	}
+	else if (n < 10)
+		ft_putchar('0' + n);
 	else
 	{
-		ft_putnbr(ft_abs(n / 10));
-		ft_putchar('0' + ft_abs(n % 10));
+		ft_putnbr(n / 10);
+		ft_putchar('0' + n % 10);
 	}
 }
